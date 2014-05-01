4a310f483cc16a5984cb8ee4064ca3f7ef8cd6be
BlueVajra <cory.leistikow@gmail.com>
2014-05-01 10:09:54 -0600

    first pass at iterating warmup

--- iterating-arrays-of-hashes/dogs.rb
+++ iterating-arrays-of-hashes/dogs.rb
@@ -1,11 +1,11 @@
 class Dogs
-  POOR      = (0..5).to_a.sample
-  AVERAGE   = (6..10).to_a.sample
+  POOR = (0..5).to_a.sample
+  AVERAGE = (6..10).to_a.sample
   EXCELLENT = (11..15).to_a.sample
 
   def initialize
-    joe   = {
-      :name => {:first => "Joe", :last=> "Smith"},
+    joe = {
+      :name => {:first => "Joe", :last => "Smith"},
       :owner_quality => EXCELLENT
     }
     sarah = {
@@ -29,30 +29,48 @@ class Dogs
   # only edit below this line
 
   def small_dogs
-    # only use the @dogs instance variable
+    @dogs.select { |dog| dog[:size] == :small }
   end
 
   def huge_dog
-    # only use the @dogs instance variable
+    huge_dogs.map do |huge_dog|
+      huge_dog.select {|k,v| k == :name}
+    end
+  end
+
+  def huge_dogs
+    @dogs.select { |dog| dog[:size] == :huge }
+  end
+
+  def large_dogs
+    @dogs.select { |dog| dog[:size] == :large }
   end
 
   def large_dog_names
-    # only use the @dogs instance variable
+   large_dogs.map { |dog| dog[:name]}
   end
 
   def joes_large_dogs
-    # only use the @dogs instance variable
+    large_dogs.select { |dog| dog[:owner][:name][:first] == 'Joe'}.map { |dog| dog[:name]}
   end
 
   def sizes
-    # only use the @dogs instance variable
+    @dogs.map {|dog| dog[:size]}.uniq
   end
 
   def owners
-    # only use the @dogs instance variable
+    uniq_owners.map {|owner| owners_full_name(owner)}
+  end
+
+  def uniq_owners
+    @dogs.map {|dog| dog[:owner]}.uniq
+  end
+
+  def owners_full_name(owner)
+    owner[:name][:first] + " " + owner[:name][:last]
   end
 
   def average_owners
-    # only use the @dogs instance variable and constants
+    uniq_owners.select{ |owner| owner[:owner_quality] == AVERAGE}.map {|owner| owners_full_name(owner)}
   end
 end
