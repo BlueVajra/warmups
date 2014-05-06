db2a03dc2ce80061e53c2b9cd9523db61384b7ef
BlueVajra <cory.leistikow@gmail.com>
2014-05-06 16:54:27 -0600

    user can get the body of the response

--- parsing-http/bin/http_response.rb
+++ parsing-http/bin/http_response.rb
@@ -1,13 +1,18 @@
 class HttpResponse
   def initialize(response)
     @response = response
+    @response_array = @response.split("\n\n")
   end
 
   def headers
-    response_array = @response.split("\n\n")
-    headers_array = response_array.first.split("\n")
+
+    headers_array = @response_array.first.split("\n")
     headers_array.shift
     headers_array.map! { |header| header.split(": ") }
     headers_array.to_h
+  end
+
+  def body
+    @response_array.last
   end
 end
--- parsing-http/spec/http_response_spec.rb
+++ parsing-http/spec/http_response_spec.rb
@@ -40,4 +40,23 @@ describe HttpResponse do
     expect(actual).to eq expected
 
   end
+
+  it "returns the body of the response" do
+    response = HttpResponse.new(RESPONSE1)
+    actual = response.body
+    expected = %{<!DOCTYPE html>
+          <html lang="en">
+<head><meta charset="utf-8" />
+<meta name="description" content="should i test private methods?" />
+<meta name="keywords" content="test,private,methods,oo,object,oriented,tdd" />
+<title>Should I Test Private Methods?</title>
+    </head>
+<body>
+<div style='font-size: 96px; font-weight: bold; text-align: center; padding-top: 200px; font-family: Verdana, Helvetica, sans-serif'>NO</div>
+      <!-- Every time you consider testing a private method, your code is telling you that you haven't allocated responsibilities well.  Are you listening to it? -->
+    </body>
+</html>}
+
+    expect(actual).to eq expected
+  end
 end
